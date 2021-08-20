7fa482914e792a5d017b1ec4cb5c0f65ebb8dbf0
Sanjai-R <samsanjai777@gmail.com>
2021-08-20 23:16:14 +0530

    card addded

--- hackathon-Client/src/App.js
+++ hackathon-Client/src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <>
       <Provider store={store}>
-        
         <AppRouter />
       </Provider>
     </>
--- hackathon-Client/src/Pages/Auth/style.js
+++ hackathon-Client/src/Pages/Auth/style.js
@@ -7,11 +7,12 @@ export const CardStyles = makeStyles((theme) => {
       boxShadow: "0px 0px 12px #00000029",
       borderRadius: "10px",
       opacity: 1,
+      marginTop:"40px",
       position: "absolute",
       top: "50%",
       left: "50%",
       transform: "translate(-50%,-50%)",
-      [theme.breakpoints.down("md")]: {
+      [theme.breakpoints.down("sm")]: {
         width: "300px",
         margin: "5% 0px"
       }
