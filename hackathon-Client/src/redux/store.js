5b1495aefda230093b946328a7fab9def554db55
Sanjai-R <samsanjai777@gmail.com>
2021-08-21 12:07:40 +0530

    store changed

--- hackathon-Client/src/redux/store.js
+++ hackathon-Client/src/redux/store.js
@@ -1,7 +1,6 @@
-import { createStore } from "redux";
-import reducer from './reducers/'
- const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "./reducers/";
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
