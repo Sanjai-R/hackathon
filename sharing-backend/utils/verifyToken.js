5c4c9ed1721674e88124e09b9eb569427f094d6d
riyazur razak <riyazurrazaknoushath786@gmail.com>
2021-10-03 10:19:22 +0530

    redirect bug fixed

--- sharing-backend/utils/verifyToken.js
+++ sharing-backend/utils/verifyToken.js
@@ -1,26 +1,45 @@
 import User from "../auth/Models/User.js";
 import jwt from "jsonwebtoken";
 
-const CLIENT_DOMAIN = "http://localhost:3000";
+const CLIENT_DOMAIN = "https://share-appfrontend.vercel.app";
 
 const VerifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
-  if (authHeader === "Bearer null")
-    return res.redirect(`${CLIENT_DOMAIN}/Signin`);
-
+  if (authHeader === "Bearer null") {
+    console.log("head null");
+    res.json({
+      success: false,
+    });
+    return;
+  }
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.redirect(`${CLIENT_DOMAIN}/Signin`);
+  if (token == null) {
+    console.log("null");
+    res.json({
+      success: false,
+    });
+    return;
+  }
 
   jwt.verify(token, "grjaejrg@jfg", (err, data) => {
-    if (err) return res.redirect(`${CLIENT_DOMAIN}/Signin`);
+    if (err) {
+      console.log("invalid");
+      res.json({
+        success: false,
+      });
+      return;
+    }
     User.findOne({ username: data.user })
       .then((user) => {
         req.user = user;
         next();
       })
       .catch((err) => {
-        res.redirect(`${CLIENT_DOMAIN}/Signin`);
+        console.log("catch");
+        res.json({
+          success: false,
+        });
       });
   });
 };
